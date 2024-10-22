e166ab59607bd9183cfe1571c9717bdaaa329d3a
Tales Vieira <devtalesvieira@gmail.com>
2024-10-22 13:51:12 -0300

    MUNDO 2 FINALIZADO
    Lista completa dos exercícios:
    36 = Aprovando Empréstimo.
    37 = Conversos de bases numéricas.
    38 = Comparando números.
    39 = Alistamento militar.
    40 = Aquele clássico de média.
    41 = Classificando atletas.
    42 = Analisando triângulos v2.0.
    43 = Índice de massa corporal.
    44 = Gerenciador de pagamentos.
    45 = Pedra, papel e tesoura.
    46 = Contagem regressiva.
    47 = Contagem de pares
    48 = Soma ímpares múltiplos de 3.
    49 = Tabuada V2.0.
    50 = Soma dos pares.
    51 = Progressão aritmética.
    52 = Números primos.
    53 = Detector de palíndromo.
    54 = Grupo da maioridade.
    55 = Maior e menor da sequência.
    56 = Analisador completo.
    57 = Validação de dados.
    58 = Jogo da adivinhação v2.0.
    59 = Criando um menu de opções.
    60 = Cálculo fatorial.
    61 = Progressão aritmética v2.0.
    62 = Progressão aritmética v3.0.
    63 = Sequência de Fibonacci v1.0.
    64 = Tratando vários valores v1.0.
    65 = Maior e menor valores.
    66 = vários números com flag.
    67 = Tabuada v3.0.
    68 = Jogo do par ou ímpar.
    69 = Análise de dados do grupo.
    70 = Estatísticas em produtos.
    71 = Simulador de caixa eletrônico.

--- MUNDO 2/009 Estrutura de repetição while/057 Exercício.py
+++ MUNDO 2/009 Estrutura de repetição while/057 Exercício.py
@@ -3,7 +3,7 @@
 # Caso esteja errado, peça a digitação novamente até ter um valor correto
 
 # Solicita a entrada inicial para o sexo, e armazena apenas o primeiro caractere, removendo espaços e convertendo para maiúsculo
-sexo = str(input('Informe seu sexo: [M/F] ')).strip().upper()[0]
+sexo = str(input('Informe seu sexo:  [M/F] ')).strip().upper()[0]
 
 # Enquanto o valor informado não for 'M' ou 'F', continua pedindo uma nova entrada
 while sexo not in 'MF':
